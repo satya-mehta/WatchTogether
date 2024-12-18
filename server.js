104b792a50017a5c5ea9020357f6fa1379dc9030
Satya Mehta <satyamehta577@gmail.com>
2024-12-19 01:42:40 +0530

    hosted a Basic Server

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 app.use(express.static("frontend"));
+
+//handle Socket.Io connections
+io.on("connection", Socket => {
+    console.log("New user connected");
+        Socket.on("disconnect", () => {
+            console.log("user Disconnected");
+        });
+});
+
+server.listen(3000, () =>{
+    console.log("Server is running on local ip and 3000 port");
+});
